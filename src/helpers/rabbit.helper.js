d1ffc6441a021b6166596aa032fa64bd259b2491
Benjamin Blumenfeld-Jones <BensThoughts@GMail.com>
2019-02-08 23:54:30 -0700

    rabbit prety print

--- src/helpers/rabbit.helper.js
+++ src/helpers/rabbit.helper.js
@@ -13,147 +13,149 @@ async function asyncForEach(array, callback) {
 
 class RabbitMQ {
 
-    constructor() {
-      this.rabbitConn;
-      this.channels = new Map();
-    }
-  
-    connect(opts, cb) {
-      amqp.connect(opts.connection, (err, conn) => {
-        let protocol = opts.connection.protocol;
-        let hostname = opts.connection.hostname;
-        let port = opts.connection.port;
-        logger.info('Connected to RabbitMQ: ' + protocol + '://' + hostname + ':' + port);
-
-        this.rabbitConn = conn;
-        this.parseOpts(opts, cb).then(() => {
-          logger.trace('Chanels opened: ' + this.channels.size);
-          if (cb) {
-            cb(err, conn);
-          }
-        }).catch((err) => logger.error(err));
-      });
-    };
-
-    async parseOpts(opts, cb) {
-
-      if (opts.exchanges) {
-        await asyncForEach(opts.exchanges, async (exchange) => {
-            await this.addExchange(exchange.channel, exchange.name, exchange.type, exchange.options);
-        });
-      } 
-
-      if (opts.queues) {
-        await asyncForEach(opts.queues, async (queue) => {
-            await this.addQueue(queue.channel, queue.name, queue.options);
-        });
-      } 
-      if (opts.bindings) {
-        await asyncForEach(opts.bindings, async (binding) => {
-            await this.bindQueue(binding.channel, binding.queue, binding.exchange, binding.key, binding.options)
-        });
-      }  
-    }
-
-    async addExchange(channel, exName, type, options, cb) {
-      await this.getChannel(channel, (err, ch) => {
-        ch.assertExchange(exName, type, options, (err, ex) => {
-          let exInfo = utils.inspect(ex);
-          logger.info('assertExchange on channel ' + channel + ': ' + exInfo);
-          if (cb) {
-            cb(err, ex);
-          }
-        });
-      });
-    };
-
-    async addQueue(channel, qName, options, cb) {
-      await this.getChannel(channel, (err, ch) => {
-        ch.assertQueue(qName, options, (err, q) => {
-          let qInfo = utils.inspect(q);
-          logger.info('assertQueue on channel ' + channel + ': ' + qInfo);
-          if (cb) {
-            cb(err, q);
-          }
-        });
-      });
-    };
-
-    async bindQueue(channel, qName, exName, key, options, cb) {
-        await this.getChannel(channel, (err, ch) => {
-          ch.bindQueue(qName, exName, key, options, (err, ok) => {
-            logger.info('bind Queue ' + qName + ' to ' + exName + ' on channel ' + channel);
-            if (cb) {
-              cb(err, ok);
-            }
-          });
-        })
-    };
-
-    createConfirmChannelPromise(channelName) {
-      return new Promise((resolve, reject) => {
-        this.rabbitConn.createConfirmChannel((err, ch) => {
-          if (err) {
-            reject(err);
-          }
-          this.setChannel(channelName, ch);
-          logger.info('Created Confirm Channel: ' + channelName);
-          resolve(ch);
-        })
-      })
-    }
-
-    async getChannel(channelName, cb) {
-      let channel = this.channels.get(channelName);
-      if (channel === undefined) {
-        await this.createConfirmChannelPromise(channelName).then((ch) => {
-          cb(undefined, ch); 
-        }).catch(err => {
-          logger.error('Error creating channel: ' + err);
-          cb(err, undefined);
-        });
-      } else {
-        cb(undefined, channel);
-      }
-    }
-
-    setChannel(channelName, ch) {
-      this.channels.set(channelName, ch);
-    }
-
-    setChannelPrefetch(channel, prefetch) {
-      this.getChannel(channel, (err, ch) => {
-        ch.prefetch(prefetch);
-      });
-    }
-
-    publish(channel, exName, msg, routingKey, options) {
-      msg = JSON.stringify(msg);
-      this.getChannel(channel, (err, ch) => {
-        ch.publish(exName, routingKey || '', new Buffer(msg));
-      })
-    };
-
-    consume(channel, qName, options) {
-
-     this.getChannel(channel, (err, ch) => {
-       let optionsMsg = utils.inspect(options);
-      logger.info('Listenting on channel ' + channel + ' to: ' + qName + ' with options: ' + optionsMsg);
-       ch.consume(qName, (msg) => {
-         let message = new RabbitMsg(msg);
-         message.deserialize();
-         this.emit('received ' + channel, message);          
-       }, options)
-     });
-   };
-
-    ack(channel, msg) {
-      msg = msg.getMsg();
-      this.getChannel(channel, (err, ch) => {
-        ch.ack(msg);
-      })
-    };
+  constructor() {
+    this.rabbitConn;
+    this.channels = new Map();
   }
+  
+  connect(opts, cb) {
+    amqp.connect(opts.connection, (err, conn) => {
+      let protocol = opts.connection.protocol;
+      let hostname = opts.connection.hostname;
+      let port = opts.connection.port;
+      logger.info('Connected to RabbitMQ: ' + protocol + '://' + hostname + ':' + port);
+
+      this.rabbitConn = conn;
+      this.parseOpts(opts, cb).then(() => {
+      logger.trace('Chanels opened: ' + this.channels.size);
+        if (cb) {
+          cb(err, conn);
+        }
+      }).catch((err) => logger.error(err));
+    });
+  };
+
+  async parseOpts(opts, cb) {
+
+    if (opts.exchanges) {
+      await asyncForEach(opts.exchanges, async (exchange) => {
+        await this.addExchange(exchange.channel, exchange.name, exchange.type, exchange.options);
+      });
+    } 
+
+    if (opts.queues) {
+      await asyncForEach(opts.queues, async (queue) => {
+        await this.addQueue(queue.channel, queue.name, queue.options);
+      });
+    } 
+    
+    if (opts.bindings) {
+      await asyncForEach(opts.bindings, async (binding) => {
+        await this.bindQueue(binding.channel, binding.queue, binding.exchange, binding.key, binding.options);
+      });
+    }  
+  
+  }
+
+  async addExchange(channel, exName, type, options, cb) {
+    await this.getChannel(channel, (err, ch) => {
+      ch.assertExchange(exName, type, options, (err, ex) => {
+        let exInfo = utils.inspect(ex);
+        logger.info('assertExchange on channel ' + channel + ': ' + exInfo);
+        if (cb) {
+          cb(err, ex);
+        }
+      });
+    });
+  }
+
+  async addQueue(channel, qName, options, cb) {
+    await this.getChannel(channel, (err, ch) => {
+      ch.assertQueue(qName, options, (err, q) => {
+        let qInfo = utils.inspect(q);
+        logger.info('assertQueue on channel ' + channel + ': ' + qInfo);
+        if (cb) {
+          cb(err, q);
+        }
+      });
+    });
+  }
+
+  async bindQueue(channel, qName, exName, key, options, cb) {
+    await this.getChannel(channel, (err, ch) => {
+      ch.bindQueue(qName, exName, key, options, (err, ok) => {
+        logger.info('bind Queue ' + qName + ' to ' + exName + ' on channel ' + channel);
+        if (cb) {
+          cb(err, ok);
+        }
+      });
+    });
+  }
+
+  createConfirmChannelPromise(channelName) {
+    return new Promise((resolve, reject) => {
+      this.rabbitConn.createConfirmChannel((err, ch) => {
+        if (err) {
+          reject(err);
+        }
+        this.setChannel(channelName, ch);
+        logger.info('Created Confirm Channel: ' + channelName);
+        resolve(ch);
+      });
+    });
+  }
+
+  setChannel(channelName, ch) {
+    this.channels.set(channelName, ch);
+  }
+
+  async getChannel(channelName, cb) {
+    let channel = this.channels.get(channelName);
+    if (channel === undefined) {
+      await this.createConfirmChannelPromise(channelName).then((ch) => {
+        cb(undefined, ch); 
+      }).catch(err => {
+        logger.error('Error creating channel: ' + err);
+        cb(err, undefined);
+      });
+    } else {
+      cb(undefined, channel);
+    }
+  }
+
+  async setChannelPrefetch(channel, prefetch) {
+    await this.getChannel(channel, (err, ch) => {
+      ch.prefetch(prefetch);
+    });
+  }
+
+  async publish(channel, exName, msg, routingKey, options) {
+    msg = JSON.stringify(msg);
+    await this.getChannel(channel, (err, ch) => {
+      ch.publish(exName, routingKey || '', new Buffer(msg));
+    });
+  }
+
+  async consume(channel, qName, options) {
+    await this.getChannel(channel, (err, ch) => {
+      let optionsMsg = utils.inspect(options);
+      logger.info('Listenting on channel ' + channel + ' to: ' + qName + ' with options: ' + optionsMsg);
+      ch.consume(qName, (msg) => {
+        let message = new RabbitMsg(msg);
+        message.deserialize();
+        this.emit('received ' + channel, message);          
+      }, options)
+    });
+  }
+
+  async ack(channel, msg) {
+    msg = msg.getMsg();
+    await this.getChannel(channel, (err, ch) => {
+      ch.ack(msg);
+    })
+  }
+
+}
 
 class RabbitMsg {
   constructor(msg) {
@@ -199,15 +201,3 @@ exports.ack = function ack(channel, msg) {
 exports.setChannelPrefetch = function setChannelPrefetch(channel, prefetch) {
   thisRabbit.setChannelPrefetch(channel, prefetch);
 }
-
-/* exports.addQueue = function addQueue(qName, options, cb) {
-  thisRabbit.addQueue(qName, options, cb);
-}
-
-exports.bindQueue = function bindQueue(qName, exName, key) {
-  thisRabbit.bindQueue(qName, exName, key);
-}
-
-exports.createConfirmChannel = function createConfirmChannel(channel, cb) {
-  thisRabbit.createConfirmChannel(channel, cb);
-} */
