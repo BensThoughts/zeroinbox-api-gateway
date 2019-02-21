fd1d44bd87eac8ec83c8083f41f4ca4ffd35bd89
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-02-21 16:51:50 -0700

    new docker.sh

--- docker.sh
+++ docker.sh
@@ -19,8 +19,8 @@ where:
 	-p, --push:  push a new version (patch, minor, or major)"
 
 
-OPTIONS=hibp
-LONGOPTS=help,info,build,push
+OPTIONS=hibp:
+LONGOPTS=help,info,build,push:,
 
 # -use ! and PIPESTATUS to get exit code with errexit set
 # -temporarily store output to be able to check for errors
@@ -35,7 +35,7 @@ fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
 
-h=n b=n p=n i=n pushMode=-
+h=n b=n i=n pushMode=-
 # now enjoy the options in order and nicely split until we see --
 while true; do
     case "$1" in
@@ -78,24 +78,16 @@ if [ "$i" = "y" ]; then
 fi
 
 if [ "$b" = "y" ]; then
-    printf "Building docker image: \n"
 	GIT_VER=$(git rev-parse @)
-	PREV_GIT_VER=$(git rev-parse @~)
 	PREV_ID=$(docker images -f reference=$IMG_NAME:latest --format "{{.ID}}")
-	printf "Image: $IMG_NAME:$GIT_VER\n"
+	# Start by removing all previous versions of the image
+	if [ ! -z PREV_ID ]; then
+		docker image rm -f "$PREV_ID"
+	fi
+	# Build and tag the image with the git commit hash
+	printf "Building docker image: $IMG_NAME:$GIT_VER\n"
 	docker build -t $IMG_NAME:latest .
 	docker tag $IMG_NAME:latest $IMG_NAME:$GIT_VER
-	PREV_GIT_TAGS=$(docker images -f reference=$IMG_NAME -f before=$IMG_NAME:$GIT_VER --format "{{.Tag}}")
-	CUR_ID=$(docker images -f reference=$IMG_NAME:latest --format "{{.ID}}")
-	# If the ID changes the image has changed so we remove the old image
-	if [ "$PREV_ID" != "$CUR_ID" ]; then
-		docker image rm $PREV_ID
-	# If the image hasn't changed but new commits have been made we remove
-	# the older commit tags and keep the image
-	elif [ ! -z "$PREV_GIT_TAGS" ]; then
-		printf "$PREV_GIT_TAGS"
-		# docker image rm "$IMG_NAME:$PREV_GIT_TAGS"
-	fi
 fi
 
 if [ "$pushMode" != "-" ]; then
@@ -115,14 +107,22 @@ if [ "$pushMode" != "-" ]; then
     esac
 	
 	printf "New sem version: $NEW_SEM_VER\n"
+	
+	# Tag the new commit version and remove the old tag
+	# (npm version bump creates a new commit)
 	NEW_GIT_VER=$(git rev-parse @)
 	PREV_GIT_VER=$(git rev-parse @~)
 	docker tag $IMG_NAME:latest $IMG_NAME:$NEW_GIT_VER
 	docker image rm $IMG_NAME:$PREV_GIT_VER
+	
+	# Push the newest commit, and latest tag
 	printf "Pushing git version to docker repo: $NEW_GIT_VER"
 	docker push $IMG_NAME:$NEW_GIT_VER
 	printf "Pushing tag latest to docker repo:"
 	docker push $IMG_NAME:latest
+
+	# Create the semantic version tag, push it, then remove it
+	# from the local system.
 	printf "Pushing tag $NEW_SEM_VER to docker repo:"
 	docker tag $IMG_NAME:$NEW_GIT_VER $IMG_NAME:$NEW_SEM_VER
 	docker push $IMG_NAME:$NEW_SEM_VER
