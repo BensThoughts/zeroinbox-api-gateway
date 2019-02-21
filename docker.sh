2101133c8787ca9a618db73a21a8b07f8fe07d24
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-02-21 14:47:18 -0700

    test

--- docker.sh
+++ docker.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-usage="$(basename "$0") [-h] [-b] [-p 'patch' || 'minor'] -- program to build and push docker container images"
+usage="$(basename "$0") [-h] [-b] [-p push patch] [-m push minor] -- program to build and push docker container images"
 help="
 where:
 	-h  help
 	-b  build container
-	-p  push (default: patch)"
+	-p  push a new patch version
+	-m  push a new minor version"
 
 echo "$usage"
 
-push="patch"
-APP_NAME="zeroinbox-api"
-REPO="gcr.io/zero-inbox-organizer"
-IMG_NAME="$REPO/$APP_NAME"
+source ./deploy.env
+IMG_NAME="$DOCKER_REPO/$APP_NAME"
 
 while getopts ':hbp:' option; do
   case "$option" in
@@ -28,20 +27,28 @@ while getopts ':hbp:' option; do
 			 docker build -t $IMG_NAME:latest .
 			 docker tag $IMG_NAME:latest $IMG_NAME:$GIT_VER
 			 CUR_ID=$(docker images -f reference=$IMG_NAME:latest --format "{{.ID}}")
-			 printf "Previoud ID: $PREV_ID\n"
-			 printf "Current ID: $CUR_ID\n"
-			 printf "Previous Git Tag: $PREV_GIT_TAG\n"
 			 if [ "$PREV_ID" != "$CUR_ID" ]; then
-					echo New Image found
 					docker image rm $PREV_ID
 			 elif [ ! -z "$PREV_GIT_TAG" ]; then
-			 		echo Previous git version found
-					echo "$PREV_GIT_TAG"
 					docker image rm "$IMG_NAME:$PREV_GIT_TAG"
 			 fi
 			 exit
        ;;
-    p) push=$OPTARG
+    p) printf "Pushing new patch version:\n"
+			 PREV_SEM_VER="v$(jq -rM '.version' package.json)"
+			 NEW_SEM_VER=$(npm version patch)
+			 printf "Previous sem version: $PREV_SEM_VER\n"
+			 printf "New sem version: $NEW_SEM_VER\n"
+			 NEW_GIT_VER=$(git rev-parse @)
+			 PREV_GIT_VER=$(git rev-parse @~)
+			 docker tag $IMG_NAME:latest $IMG_NAME:$NEW_GIT_VERSION
+			 docker image rm $IMG_NAME:$PREV_GIT_VERSION
+			 docker push $IMG_NAME:$NEW_GIT_VERSION
+			 docker push $IMG_NAME:latest
+			 docker tag $IMG_NAME:$NEW_GIT_VERSION $IMG_NAME:$NEW_SEM_VERSION
+			 docker push $IMG_NAME:$NEW_SEM_VERSION
+			 docker image rm $IMG_NAME:$NEW_SEM_VERSION
+			 exit
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
