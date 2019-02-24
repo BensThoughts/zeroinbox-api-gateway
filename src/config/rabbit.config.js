f3cafc6e04f141bc7c6d35aed795c499bdf3008d
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-02-23 20:58:40 -0700

    rabbit url optoin via env

--- src/config/rabbit.config.js
+++ src/config/rabbit.config.js
@@ -16,18 +16,29 @@ const RABBIT_USERNAME = process.env.RABBIT_USERNAME;
 const RABBIT_PASSWORD = process.env.RABBIT_PASSWORD;
 const RABBIT_PROTOCOL = process.env.RABBIT_PROTOCOL;
 
-const rabbit_config = {
-    connection: {
-        hostname: RABBIT_HOSTNAME,
-        port: RABBIT_PORT,
-        username: RABBIT_USERNAME,
-        password: RABBIT_PASSWORD,
-        protocol: RABBIT_PROTOCOL,
-        frameMax: RABBIT_FRAME_MAX,
-        vhost: RABBIT_VHOST,
-        heartbeat: RABBIT_HEARTBEAT,
-    },
-    ...rabbitTopology
+const RABBIT_URL = process.env.RABBIT_URL;
+
+let rabbit_config;
+
+if (RABBIT_HOSTNAME) {
+    rabbit_config = {
+        connection: {
+            hostname: RABBIT_HOSTNAME,
+            port: RABBIT_PORT,
+            username: RABBIT_USERNAME,
+            password: RABBIT_PASSWORD,
+            protocol: RABBIT_PROTOCOL,
+            frameMax: RABBIT_FRAME_MAX,
+            vhost: RABBIT_VHOST,
+            heartbeat: RABBIT_HEARTBEAT,
+        },
+        ...rabbitTopology
+    }
+} else if (RABBIT_URL) {
+    rabbit_config = {
+        url: RABBIT_URL,
+        ...rabbitTopology
+    }
 }
 
 module.exports = {
