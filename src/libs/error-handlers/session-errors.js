5a620a2169a0effb2822dcfd5441a21e8b41d646
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-03-11 03:31:30 -0700

    added logging of req.cookie if req.session turns undefined

--- src/libs/error-handlers/session-errors.js
+++ src/libs/error-handlers/session-errors.js
@@ -2,7 +2,8 @@ const logger = require('../loggers/log4js');
 
 function sessionErrors(req, res, next) {
   if (!req.session) {
-    logger.debug('No session set, is redis up? or attacker?!');
+    logger.error('No session set, is redis up? or attacker?!');
+    logger.error('No session set cookie: ' + req.cookie);
     res.status(403).json({
       status: 'error',
       status_message: 'No session found, was cookie set correctly?!'
