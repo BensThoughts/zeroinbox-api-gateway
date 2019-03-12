ed6ceef7af192c753eba8854277239d6fcd69534
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-03-11 18:00:52 -0700

    more specific error message

--- src/libs/error-handlers/session-errors.js
+++ src/libs/error-handlers/session-errors.js
@@ -3,7 +3,7 @@ const logger = require('../loggers/log4js');
 function sessionErrors(req, res, next) {
   if (!req.session) {
     logger.error('No session set, is redis up? or attacker?!');
-    logger.error('No session set cookie: ' + req.cookie);
+    logger.error('Cookie attached to req with no session set: ' + req.cookie);
     res.status(403).json({
       status: 'error',
       status_message: 'No session found, was cookie set correctly?!'
