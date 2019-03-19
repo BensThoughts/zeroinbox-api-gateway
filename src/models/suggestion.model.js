9129014b29bc1f49c947c8648143934ef41ec13e
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-03-18 21:58:50 -0700

    added parity to batch with unsubscribeList

--- src/models/suggestion.model.js
+++ src/models/suggestion.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const suggestionSchema = new Schema({
   userId: { type: String, required: true },
   senderId: { type: String, required: true },
+  subscriptionList: { type: Boolean, required: false },
   delete: { type: Boolean, required: true },
   labelByName: { type: Boolean, required: true },
   labelBySize: { type: Boolean, required: true },
