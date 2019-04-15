997de9c07ba41f892409a5de524753fe46bb8aab
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-04-15 14:18:28 -0700

    changed action model

--- src/models/action.model.js
+++ src/models/action.model.js
@@ -4,7 +4,23 @@ const Schema = mongoose.Schema;
 const actionSchema = new Schema({
   userId: { type: String, required: true },
   senderId: { type: String, required: true },
-  action: { type: String, required: true }
+  action: { 
+    type: String, 
+    required: [true, 'Action of label or delete is required!'],
+    validate: {
+      validator: function(action) {
+        switch(action) {
+          case 'label':
+            return true;
+          case 'delete':
+            return true;
+          default:
+            return false;
+        }
+      },
+      message: props => `${props.value} is not a valid action (try label or delete)!` 
+    }
+  }
 });
 
 const Action = mongoose.model('action', actionSchema);
