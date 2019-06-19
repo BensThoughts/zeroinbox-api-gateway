ffe7593932efaf8cb491791936887fd04faae6fa
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-06-19 10:49:40 -0700

    added categories model

---
+++ src/models/categories.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  name: { type: String, required: true },
+  value: { type: String, required: true }
+});
+
+const categoriesSchema = new Schema({
+  userId: { type: String, required: true },
+  categories: { type: [categorySchema], required: true }
+});
+
+const Categories = mongoose.model('Categories', categoriesSchema);
+
+module.exports = Categories;
