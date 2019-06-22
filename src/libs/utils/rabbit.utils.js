1a631382122d7e2ba0a726b57a4865e5e0098708
Benjamin Blumenfeld-Jones <SanFranciscoSunrise@GMail.com>
2019-06-22 12:00:57 -0700

    cleaned up rabbit utils

--- src/libs/utils/rabbit.utils.js
+++ src/libs/utils/rabbit.utils.js
@@ -1,9 +1,29 @@
 const rabbit = require('zero-rabbit');
 const logger = require('../loggers/log4js');
+const {
+  rabbit_topology
+} = require('../../config/rabbit.config');
 
-exports.publishUser = function(userId, access_token) {
+exports.publishGetMessagesUserId = function (userId, access_token) {
+  let sentAt = new Date().getTime();
+  let getMessagesExchange = rabbit_topology.exchanges.fanout.getMessages;
+  rabbit.publish(rabbit_topology.channels.send, getMessagesExchange, '', {
+    userId: userId,
+    access_token: access_token,
+  }, { 
+    contentType: 'application/json', 
+    type: 'user',
+    appId: 'zi-api-gateway',
+    timestamp: sentAt,
+    encoding: 'string Buffer',
+    persistent: true,
+  });
+}
+
+exports.publishGetThreadsUserId = function(userId, access_token) {
     let sentAt = new Date().getTime();
-    rabbit.publish('api.send.1', 'user.ids.ex.1', '', {
+    let getThreadsExchange = rabbit_topology.exchanges.fanout.getThreads;
+    rabbit.publish(rabbit_topology.channels.send, getThreadsExchange, '', {
       userId: userId,
       access_token: access_token,
     }, { 
@@ -15,22 +35,6 @@ exports.publishUser = function(userId, access_token) {
       persistent: true,
     });
 }
-
-
-function publishActionsUserId(userId) {
-  let sentAt = new Date().getTime();
-  rabbit.publish('api.send.1', 'actions.userIds.ex.1', '', {
-    userId: userId
-  },
-  {
-  contentType: 'application/json',
-  type: 'actions',
-  appId: 'zi-api-gateway',
-  timestamp: sentAt,
-  encoding: 'string Buffer',
-  persistent: true
-  });
-};
 
 /**
  * actionsObj - {
@@ -52,15 +56,8 @@ exports.publishActions = function(userId, access_token, actionsObj, senderIds) {
     let filter = actionsObj.filter;
     let labelName = actionsObj.labelName;
     let category = actionsObj.category;
-
-    
     let unsubscribeEmail = actionsObj.unsubscribeEmail;
     let unsubscribeWeb = actionsObj.unsubscribeWeb;
-
-/*     let lastMsg = false;
-    if (i === (senderIds.length -1)) {
-      lastMsg = true;
-    } */
 
     rabbit.publish('api.send.1', 'actions.direct.ex.1', '', {
         userId: userId,
@@ -72,7 +69,6 @@ exports.publishActions = function(userId, access_token, actionsObj, senderIds) {
         category: category,
         unsubscribeEmail: unsubscribeEmail,
         unsubscribeWeb: unsubscribeWeb,
-        // lastMsg: lastMsg,
     }, {
         contentType: 'application/json',
         type: 'actions',
@@ -83,52 +79,3 @@ exports.publishActions = function(userId, access_token, actionsObj, senderIds) {
     });
   } 
 }
-  /*   rabbit.assertQueue('api.send.1', 'actions.userId.' + userId, { autoDelete: false, durable: true }, (assertQueueErr, q) => {
-      if (assertQueueErr) {
-        return logger.error(assertQueueErr);
-      } else {
-        rabbit.bindQueue('api.send.1', 'actions.userId.' + userId, 'actions.topic.ex.1', 'userId.' + userId, {}, (bindQueueErr, ok) => {
-          if (bindQueueErr) {
-            return logger.error(bindQueueErr);
-          } else {
-            for (i = 0; i < senderIds.length; i++) {
-              let senderId = senderIds[i];
-              let sentAt = new Date().getTime();
-              let actionType = actionsObj.actionType;
-              let filter = actionsObj.filter;
-              let labelName = actionsObj.labelName;
-              let category = actionsObj.category;
-              let unsubscribeEmail = actionsObj.unsubscribeEmail;
-              let unsubscribeWeb = actionsObj.unsubscribeWeb;
-
-              let lastMsg = false;
-              if (i === (senderIds.length -1)) {
-                lastMsg = true;
-              }
-          
-              rabbit.publish('api.send.1', 'actions.topic.ex.1', 'userId.' + userId, {
-                  userId: userId,
-                  access_token: access_token,
-                  senderId: senderId,
-                  actionType: actionType,
-                  filter: filter,
-                  labelName: labelName,
-                  category: category,
-                  unsubscribeEmail: unsubscribeEmail,
-                  unsubscribeWeb: unsubscribeWeb,
-                  lastMsg: lastMsg,
-              }, {
-                  contentType: 'application/json',
-                  type: 'actions',
-                  appId: 'zi-api-gateway',
-                  timestamp: sentAt,
-                  encoding: 'string Buffer',
-                  persistent: true,
-              });
-              publishActionsUserId(userId);
-   
-            } 
-          }
-        });
-      }
-    }); */
