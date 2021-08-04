f623da75eed530dbdedc8001c51a9cf08525d433
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-03 22:08:06 -0700

    better config logging

--- src/config/rabbit.config.js
+++ src/config/rabbit.config.js
@@ -1,42 +1,52 @@
 const fs = require('fs');
 const path = require('path');
 const workdir = path.resolve(__dirname) + '/';
-const rabbitTop = fs.readFileSync(workdir + 'rabbit.topology.json');
-const rabbitTopology = JSON.parse(rabbitTop.toString());
-const logger = require('../libs/loggers/log4js');
-logger.info('Rabbit Topology: ' + rabbitTop.toString());
+const rabbitTopJSON = fs.readFileSync(workdir + 'rabbit.topology.json');
+const rabbitTopology = JSON.parse(rabbitTopJSON.toString());
+// const logger = require('../libs/loggers/log4js');
+// logger.info('Rabbit Topology: ' + rabbitTop.toString());
 
-
-const RABBIT_HOSTNAME = process.env.RABBIT_HOSTNAME;
-const RABBIT_PORT = process.env.RABBIT_PORT;
-const RABBIT_FRAME_MAX = process.env.RABBIT_FRAME_MAX;
-const RABBIT_HEARTBEAT = process.env.RABBIT_HEARTBEAT;
-const RABBIT_VHOST = process.env.RABBIT_VHOST;
-const RABBIT_USERNAME = process.env.RABBIT_USERNAME;
-const RABBIT_PASSWORD = process.env.RABBIT_PASSWORD;
-const RABBIT_PROTOCOL = process.env.RABBIT_PROTOCOL;
+const RABBIT_CONNECTION = {
+  RABBIT_HOSTNAME: process.env.RABBIT_HOSTNAME,
+  RABBIT_PORT: process.env.RABBIT_PORT,
+  RABBIT_FRAME_MAX: process.env.RABBIT_FRAME_MAX,
+  RABBIT_HEARTBEAT: process.env.RABBIT_HEARTBEAT,
+  RABBIT_VHOST: process.env.RABBIT_VHOST,
+  RABBIT_USERNAME: process.env.RABBIT_USERNAME,
+  RABBIT_PASSWORD: process.env.RABBIT_PASSWORD,
+  RABBIT_PROTOCOL: process.env.RABBIT_PROTOCOL,
+};
 
 const RABBIT_URL = process.env.RABBIT_URL;
+
+// const RABBIT_HOSTNAME = process.env.RABBIT_HOSTNAME;
+// const RABBIT_PORT = process.env.RABBIT_PORT;
+// const RABBIT_FRAME_MAX = process.env.RABBIT_FRAME_MAX;
+// const RABBIT_HEARTBEAT = process.env.RABBIT_HEARTBEAT;
+// const RABBIT_VHOST = process.env.RABBIT_VHOST;
+// const RABBIT_USERNAME = process.env.RABBIT_USERNAME;
+// const RABBIT_PASSWORD = process.env.RABBIT_PASSWORD;
+// const RABBIT_PROTOCOL = process.env.RABBIT_PROTOCOL;
 
 let rabbitConfig;
 
 if (RABBIT_URL) {
-  logger.info('RABBIT_URL: ' + RABBIT_URL);
+  // logger.info('RABBIT_URL: ' + RABBIT_URL);
   rabbitConfig = {
     url: RABBIT_URL,
     ...rabbitTopology,
   };
-} else if (RABBIT_HOSTNAME) {
+} else if (RABBIT_CONNECTION) {
   rabbitConfig = {
     connection: {
-      hostname: RABBIT_HOSTNAME,
-      port: RABBIT_PORT,
-      username: RABBIT_USERNAME,
-      password: RABBIT_PASSWORD,
-      protocol: RABBIT_PROTOCOL,
-      frameMax: RABBIT_FRAME_MAX,
-      vhost: RABBIT_VHOST,
-      heartbeat: RABBIT_HEARTBEAT,
+      hostname: RABBIT_CONNECTION.RABBIT_HOSTNAME,
+      port: RABBIT_CONNECTION.RABBIT_PORT,
+      username: RABBIT_CONNECTION.RABBIT_USERNAME,
+      password: RABBIT_CONNECTION.RABBIT_PASSWORD,
+      protocol: RABBIT_CONNECTION.RABBIT_PROTOCOL,
+      frameMax: RABBIT_CONNECTION.RABBIT_FRAME_MAX,
+      vhost: RABBIT_CONNECTION.RABBIT_VHOST,
+      heartbeat: RABBIT_CONNECTION.RABBIT_HEARTBEAT,
     },
     ...rabbitTopology,
   };
@@ -59,4 +69,7 @@ const userTopology = {
 module.exports = {
   rabbitConfig,
   userTopology,
+  RABBIT_CONNECTION,
+  RABBIT_URL,
+  rabbitTopJSON,
 };
--- src/libs/loggers/config.logger.js
+++ src/libs/loggers/config.logger.js
@@ -2,19 +2,49 @@ const logger = require('./log4js');
 
 const initEnvVars = require('../../config/init.config');
 const authEnvVars = require('../../config/auth.config');
+const {
+  rabbitTopJSON,
+  RABBIT_URL,
+  RABBIT_CONNECTION,
+} = require('../../config/rabbit.config');
 
 /**
  * Logs the config given by env vars
  */
 function logConfig() {
+  // Microservice Name
+  logger.info('');
+  logger.info('--------------------------------------------------------------');
+  logger.info('++                                                          ++');
+  logger.info('++                 ZeroInbox Api-Gateway                    ++');
+  logger.info('++                                                          ++');
+  logger.info('--------------------------------------------------------------');
+
   // init env vars
+  logger.info('');
+  logger.info('********INIT ENV VARS********');
   Object.keys(initEnvVars).forEach((envVar) => {
     logger.info(envVar + ': ' + initEnvVars[envVar]);
   });
   // authentication env vars
+  logger.info('');
+  logger.info('********AUTH ENV VARS********');
   Object.keys(authEnvVars).forEach((envVar) => {
     logger.info(envVar + ': ' + authEnvVars[envVar]);
   });
+
+  logger.info('');
+  logger.info('********RABBIT CONNECTION********');
+  if (RABBIT_URL) {
+    logger.info('RABBIT_URL: ' + RABBIT_URL);
+  } else if (RABBIT_CONNECTION) {
+    Object.keys(RABBIT_CONNECTION).forEach((key) => {
+      logger.info(key + ': ' + RABBIT_CONNECTION[key]);
+    });
+  }
+  logger.info('Rabbit Topology: ' + rabbitTopJSON.toString());
+
+  logger.info('');
 }
 
 module.exports = {
