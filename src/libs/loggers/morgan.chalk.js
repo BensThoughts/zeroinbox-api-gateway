62dc29e031aeef972133f4ab0be47a5578dd51ec
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-03 22:35:38 -0700

    userId added to http logger

--- src/libs/loggers/morgan.chalk.js
+++ src/libs/loggers/morgan.chalk.js
@@ -1,21 +1,33 @@
 const chalk = require('chalk');
 
 exports.logOK = function(token, req, res) {
-  return [
-    chalk.green.bold(token.method(req, res)),
-    chalk.yellow.bold(token.status(req, res)),
-    chalk.green(token.url(req, res)),
-    chalk.yellow(token['response-time'](req, res) + 'ms'),
-    chalk.white('@ ' + token.date(req, res)),
-  ].join(' ');
-};
-
-exports.logError = function(token, req, res) {
-  return [
+  let output = [''];
+  try {
+    output = [chalk.white(req.session.userInfo.userId + ' -')];
+  } catch (e) {
+    output = [''];
+  }
+  return output.concat([
     chalk.green.bold(token.method(req, res)),
     chalk.red.bold(token.status(req, res)),
     chalk.green(token.url(req, res)),
-    chalk.yellow(token['response-time'](req, res) + 'ms'),
+    chalk.yellow('- Response Time: ' + token['response-time'](req, res) + 'ms'),
     chalk.white('@ ' + token.date(req, res)),
-  ].join(' ');
+  ]).join(' ');
+};
+
+exports.logError = function(token, req, res) {
+  let output = [''];
+  try {
+    output = [chalk.white(req.session.userInfo.userId + ' -')];
+  } catch (e) {
+    output = [''];
+  }
+  return output.concat([
+    chalk.green.bold(token.method(req, res)),
+    chalk.red.bold(token.status(req, res)),
+    chalk.green(token.url(req, res)),
+    chalk.yellow('- Response Time: ' + token['response-time'](req, res) + 'ms'),
+    chalk.white('@ ' + token.date(req, res)),
+  ]).join(' ');
 };
